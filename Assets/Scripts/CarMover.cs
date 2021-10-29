d684d52edac42e0700a2b0256772c20766a326ed
çağdaş bilgin <cagdasbilgin1@gmail.com>
2021-10-29 12:12:17 +0300

    feat: Add particle system for wheel

--- Assets/Scripts/CarMover.cs
+++ Assets/Scripts/CarMover.cs
@@ -8,6 +8,9 @@ public class CarMover : MonoBehaviour
     [SerializeField] float moveSpeed = 15f;
     [SerializeField] float moveSpeedWhileTurning = 10f;
     [SerializeField] float turnSpeed = 300f;
+    [SerializeField] ParticleSystem leftWhellSmokeParticles;
+    [SerializeField] ParticleSystem rightWhellSmokeParticles;
+
     float currentMoveSpeed;
     float moverInput;
     float turnerInput;
@@ -51,6 +54,18 @@ public class CarMover : MonoBehaviour
     {
         turnerInput *= Time.fixedDeltaTime;
         transform.Rotate(0, turnerInput, 0);
+
+        if(turnerInput != 0)
+        {
+            leftWhellSmokeParticles.Play();
+            rightWhellSmokeParticles.Play();
+        }
+        else
+        {
+            leftWhellSmokeParticles.Stop();
+            rightWhellSmokeParticles.Stop();
+        }
+        
     }
 
     void HandleMoveSpeed() //if the car is turning, reduce its moveSpeed
